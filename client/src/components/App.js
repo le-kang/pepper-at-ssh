ff9f83ba59a0a8658b289b4f919c7bf16261bd44
Le Kang <lekang8817@gmail.com>
2019-02-19 13:20:37 +1100

    Change logo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,8 @@ import ForgotPassword from './ForgotPassword'
 import ResetPassword from './ResetPassword'
 import styles from '../styles/App.module.css'
 import magiclabLogo from '../assets/images/magiclab-white-logo.png'
-import sydneyStartupHubLogo from '../assets/images/sydney-startup-hub-logo.png'
+import socialRobotReasearchLogo from '../assets/images/social-robot-study-logo.png'
+// import sydneyStartupHubLogo from '../assets/images/sydney-startup-hub-logo.png'
 
 const navigations = {
   '/login': { prev: '/', name: 'Login' },
@@ -68,7 +69,7 @@ class App extends Component {
         <header key="header" className={styles.header}>
           <img alt="magiclab" src={magiclabLogo} height="120" />
           <Divider className={styles.divider} type="vertical" />
-          <img alt="sydney startup hub" src={sydneyStartupHubLogo} height="120" />
+          <img alt="sydney startup hub" src={socialRobotReasearchLogo} height="120" />
         </header>,
         <main key="main" className={styles.main}>
           {this.renderBreadcrumb()}
